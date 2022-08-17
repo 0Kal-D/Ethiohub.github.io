a2b0200e3389c69dbef69daff194d88284d51ef2
Kalofficial <88226646+Cracker1111@users.noreply.github.com>
2022-08-17 12:31:37 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+var textbook = document.getElementById('textbook');
+var techer = document.getElementById('techer-guid');
+var stnine = document.getElementById('st9');
+var stten = document.getElementById('st10');
+var steleven = document.getElementById('st11');
+var sttwelve = document.getElementById('st12');
+var tgnine = document.getElementById('tg9');
+var tgten = document.getElementById('tg10');
+var tgeleven = document.getElementById('tg11');
+var tgtwelve = document.getElementById('tg12');
+var body = document.getElementById('body');
+
+function bhide(){
+    textbook.style.display = 'none'
+}
+function bshow(){
+    textbook.style.display ='block'
+}
+function ghide(){
+    techer.style.display = 'none'
+}
+function gshow(){
+    techer.style.display ='block'
+}
+function stnineshow(){
+    stnine.style.display ='block'
+}
+function stninehide(){
+    stnine.style.display ='none'
+}
+function sttenshow(){
+    stten.style.display ='block'
+}
+function sttenhide(){
+    stten.style.display ='none'
+}
+function stelevenshow(){
+    steleven.style.display ='block'
+}
+function stelevenhide(){
+    steleven.style.display ='none'
+}
+function sttwelveshow(){
+    sttwelve.style.display ='block'
+}
+function sttwelvehide(){
+    sttwelve.style.display ='none'
+}
+
+function tgnineshow(){
+    tgnine.style.display ='block'
+}
+function tgninehide(){
+    tgnine.style.display ='none'
+}
+function tgtenshow(){
+    tgten.style.display ='block'
+}
+function tgtenhide(){
+    tgten.style.display ='none'
+}
+function tgelevenshow(){
+    tgeleven.style.display ='block'
+}
+function tgelevenhide(){
+    tgeleven.style.display ='none'
+}
+function tgtwelveshow(){
+    tgtwelve.style.display ='block'
+}
+function tgtwelvehide(){
+    tgtwelve.style.display ='none'
+}
+function down(){
+    alert("Downloading...")
+}
+    function dark(){
+        body.style.backgroundColor = 'rgb(100, 100, 100)'; 
+       
+    }
+    function light(){
+        body.style.backgroundColor = 'rgb(69, 141, 223)';
+    }
